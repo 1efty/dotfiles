873e2181edade9dbb8ec7dec744c31ff1dfe4a3c
1efty <18194777+1efty@users.noreply.github.com>
2020-10-01 00:37:49 -0500

    Add .zshrc template

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ gomplate -f templates/.gitignore -o $DOTFILES_STORAGE_PATH/.gitignore
 gomplate -f templates/.gitconfig -o $DOTFILES_STORAGE_PATH/.gitconfig
 gomplate -f templates/.bash_profile -o $DOTFILES_STORAGE_PATH/.bash_profile
 gomplate -f templates/.bashrc -o $DOTFILES_STORAGE_PATH/.bashrc
+gomplate -f templates/.zshrc -o $DOTFILES_STORAGE_PATH/.zshrc
 
 ln -sf $DOTFILES_STORAGE_PATH/.bashrc $HOME/.bashrc
 ln -sf $DOTFILES_STORAGE_PATH/.bash_profile $HOME/.bash_profile
 ln -sf $DOTFILES_STORAGE_PATH/.gitignore $HOME/.gitignore
 ln -sf $DOTFILES_STORAGE_PATH/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_STORAGE_PATH/.zshrc $HOME/.zshrc
