1c1db737f175e4765b884d3b4b0de75201d3020c
1efty <git+me@1efty.cloud>
2020-10-03 10:16:41 -0500

    Create shared rc directory

--- bash/.bashrc
+++ bash/.bashrc
@@ -5,8 +5,15 @@ if [[ $- != *i* ]]; then
 	return
 fi
 
-# Load all files from rc.d directory
-if [ -d ${HOME}/.bashrc.d ]; then
+# Load all files from .rc.d directory
+if [ -d "${HOME}/.rc.d" ]; then
+	for file in ${HOME}/.rc.d/*.sh; do
+		source $file
+	done
+fi
+
+# Load all files from .bashrc.d directory
+if [ -d "${HOME}/.bashrc.d" ]; then
 	for file in ${HOME}/.bashrc.d/*.sh; do
 		source $file
 	done
--- rc/.rc.d/git.sh
+++ rc/.rc.d/git.sh
@@ -19,19 +19,3 @@ function gitlocalconfig() {
 	git config user.name "$(git config --get user.name)"
 	git config user.email "$(git config --get user.email)"
 }
-
-# setup bash completion
-if command -v git >/dev/null; then
-	if [ "$OS" = "darwin" ]; then
-		source /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
-		source /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
-	elif [ $OS = "linux" ]; then
-		if [ -f "/usr/share/git-core/contrib/completion/git-prompt.sh" ]; then
-			# fedora
-			source /usr/share/git-core/contrib/completion/git-prompt.sh
-		elif [ -f "/usr/lib/git-core/git-sh-prompt" ]; then
-			# ubuntu
-			source /usr/lib/git-core/git-sh-prompt
-		fi
-	fi
-fi
