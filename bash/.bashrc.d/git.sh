ef506f244d7e30025975b1292cf85794bc32fa48
1efty <18194777+1efty@users.noreply.github.com>
2020-10-03 10:03:45 -0500

    Move files from .bashrc to shared .rc

--- bash/.bashrc.d/git.sh
+++ bash/.bashrc.d/git.sh
@@ -1,25 +1,3 @@
-# delete local tracking branches that are no longer on remote
-function gitmergehook() {
-	git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d 2>/dev/null
-	git fetch -p
-}
-
-# get all commiters in repository
-function gitcommitters() {
-	git log --pretty="%an %ae%n%cn %ce" | sort | uniq
-}
-
-# remove a specific file from history
-function gitremovefromhistory() {
-	git filter-branch --force --index-filter "git rm --cached --ignore-match $1" --prune-empty --tag-name-filter cat -- --all
-}
-
-# set common git config in .git/config
-function gitlocalconfig() {
-	git config user.name "$(git config --get user.name)"
-	git config user.email "$(git config --get user.email)"
-}
-
 # setup bash completion
 if command -v git >/dev/null; then
 	if [ "$OS" = "darwin" ]; then
