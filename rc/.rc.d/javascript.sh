11199ecdaa73c17a5ce04a4261e1c9fbe6e8e8cc
1efty <18194777+1efty@users.noreply.github.com>
2020-10-10 01:25:18 -0500

    Add npm

--- rc/.rc.d/javascript.sh
+++ rc/.rc.d/javascript.sh
@@ -1,10 +1,7 @@
 export NPM_PACKAGES="${HOME}/.npm-packages"
 
 # Setup a local npm package path
-if command -v npm >/dev/null; then
-	test -f "${HOME}/.npmrc" || echo "prefix=\"${NPM_PACKAGES}\"" >"${HOME}/.npmrc"
-	test -d "$NPM_PACKAGES" || mkdir "$NPM_PACKAGES"
-fi
+test -d "$NPM_PACKAGES" || mkdir "$NPM_PACKAGES"
 
 # Add local npm package bin to $PATH
 if [ -d $NPM_PACKAGES ]; then
