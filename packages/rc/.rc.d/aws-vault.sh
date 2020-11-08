ee443aa8126e3b6cc27f1de2d164dd801d747f38
1efty <18194777+1efty@users.noreply.github.com>
2020-11-08 16:13:14 -0600

    Fix assume-role function

--- packages/rc/.rc.d/aws-vault.sh
+++ packages/rc/.rc.d/aws-vault.sh
@@ -1,15 +1,13 @@
 if command -v aws-vault >/dev/null; then
 
 	export AWS_VAULT_BACKEND="file"
-	export AWS_VAULT_ASSUME_ROLE_TTL="1h"
-	export AWS_VAULT_SESSION_TTL="12h"
+	export AWS_VAULT_DURATION="1h"
 
 	if [ -n "$AWS_VAULT" ]; then
 		export ASSUME_ROLE="$AWS_VAULT"
 	else
 		AWS_VAULT_ARGS=()
-		AWS_VAULT_ARGS+=("--assume-role-ttl=${AWS_VAULT_ASSUME_ROLE_TTL}")
-		AWS_VAULT_ARGS+=("--session-ttl=${AWS_VAULT_SESSION_TTL}")
+		AWS_VAULT_ARGS+=("--duration=${AWS_VAULT_ASSUME_ROLE_TTL}")
 	fi
 
 	function _aws_vault_assume_role() {
@@ -20,7 +18,7 @@ if command -v aws-vault >/dev/null; then
 		fi
 		shift
 		if [ $# -eq 0 ]; then
-			aws-vault exec ${AWS_VAULT_ARGS[@]} "$role" -- bash -l
+			aws-vault exec ${AWS_VAULT_ARGS[@]} "$role" -- $SHELL
 		else
 			aws-vault exec ${AWS_VAULT_ARGS[@]} "$role" -- $*
 		fi
--- packages/rc/.rc.d/aws.sh
+++ packages/rc/.rc.d/aws.sh
@@ -3,26 +3,3 @@ export AWS_CONFIG_FILE="${AWS_DATA_PATH}/config"
 export AWS_SHARED_CREDENTIALS_FILE="${AWS_DATA_PATH}/credentials"
 export AWS_DEFAULT_REGION="us-east-1"
 export AWS_DEFAULT_OUTPUT="json"
-
-# setup environment for AWS communication
-function awsenv() {
-	local profile="$1"
-
-	if command -v aws >/dev/null; then
-		if aws configure list --profile "$profile" >/dev/null; then
-			export AWS_DEFAULT_PROFILE="$profile"
-			export AWS_VAULT="$profile"
-			export AWS_DEFAULT_REGION="$(
-				aws configure get region --profile "$profile"
-			)"
-			export AWS_ACCESS_KEY_ID="$(
-				aws configure get aws_access_key_id --profile "$profile"
-			)"
-			export AWS_SECRET_ACCESS_KEY="$(
-				aws configure get aws_secret_access_key --profile "$profile"
-			)"
-		else
-			unset AWS_DEFAULT_PROFILE AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_VAULT
-		fi
-	fi
-}
