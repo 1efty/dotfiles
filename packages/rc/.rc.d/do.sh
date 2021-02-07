f085a4687f962ca4ce481a136e7526ffe89ed933
1efty <git+me@1efty.cloud>
2021-02-07 09:22:14 -0600

    rc: fix doctl support on linux

--- packages/rc/.rc.d/do.sh
+++ packages/rc/.rc.d/do.sh
@@ -1,11 +1,14 @@
 if [ "${OS}" = "darwin" ]; then
 	export DIGITALOCEAN_CONFIG="${HOME}/Library/Application Support/doctl/config.yaml"
+elif [ "${OS}" = "linux" ]; then
+	export DIGITALOCEAN_CONFIG="${HOME}/.config/doctl/config.yaml"
 fi
 
 # setup environment for digitalocean communication
 function doenv() {
 	if [ -f "${DIGITALOCEAN_CONFIG}" ]; then
-		export DIGITALOCEAN_ACCESS_TOKEN="$(yq r "${HOME}/Library/Application Support/doctl/config.yaml" access-token)"
+		export DIGITALOCEAN_ACCESS_TOKEN="$(yq r "${DIGITALOCEAN_CONFIG}" access-token)"
+		export DO_TOKEN="${DIGITALOCEAN_ACCESS_TOKEN}"
 	else
 		echo "${DIGITALOCEAN_CONFIG} is not available"
 	fi
