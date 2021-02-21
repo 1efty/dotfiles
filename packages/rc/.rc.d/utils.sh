1ffb332a1a3da288dabf8c0464c3f6afde3c9755
1efty <18194777+1efty@users.noreply.github.com>
2021-02-21 02:31:12 -0600

    fix(utils): use logical OR in cclean

--- packages/rc/.rc.d/utils.sh
+++ packages/rc/.rc.d/utils.sh
@@ -1,6 +1,6 @@
 # clean up cache directories
 function cclean() {
 	if [ -d "${HOME}/.cache" ]; then
-		find "${HOME}/.cache" -name "*.png" -name "*.jpg" -exec rm {} \;
+		find "${HOME}/.cache" \( -name "*.png" -o -name "*.jpg" \) -exec rm {} \;
 	fi
 }
