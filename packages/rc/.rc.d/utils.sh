de28237a7ab5cb34ce47d1a62838426643e52cb0
1efty <18194777+1efty@users.noreply.github.com>
2021-05-17 16:27:53 -0500

    cclean: add some more things to clean up

--- packages/rc/.rc.d/utils.sh
+++ packages/rc/.rc.d/utils.sh
@@ -1,6 +1,20 @@
 # clean up cache directories
 function cclean() {
+	# clean image cache
 	if [ -d "${HOME}/.cache" ]; then
 		find "${HOME}/.cache" \( -name "*.png" -o -name "*.jpg" \) -exec rm {} \;
+	fi
+
+	# clean terminal history
+	history -c
+
+	# clean terraform cache
+	if [ -d "${HOME}/.terraform.d" ]; then
+		rm -rf "${HOME}/.terraform.d"
+	fi
+
+	# clean packer cache
+	if [ -d "${HOME}/.terraform.d" ]; then
+		rm -rf "${HOME}/.packer.d"
 	fi
 }
