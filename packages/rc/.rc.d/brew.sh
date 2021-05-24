404fc112ecb32848d83456ec1a9a623dd9a612ee
1efty <18194777+1efty@users.noreply.github.com>
2021-05-24 14:01:44 -0500

    homebrew: check home dir for installation

--- packages/rc/.rc.d/brew.sh
+++ packages/rc/.rc.d/brew.sh
@@ -1,3 +1,7 @@
-if [ -d "/home/linuxbrew/.linuxbrew" ]; then
+export HOMEBREW_NO_ANALYTICS=1
+
+if [ -d "${HOME}/.linuxbrew" ]; then
+    eval $(${HOME}/.linuxbrew/bin/brew shellenv)
+elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
 	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
