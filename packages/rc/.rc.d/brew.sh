8028dc91aab7d07743013b24fe74e1d0dd0fa143
1efty <git+me@1efty.cloud>
2021-05-25 22:13:52 -0500

    fix: lint issue with brew.sh

--- packages/rc/.rc.d/brew.sh
+++ packages/rc/.rc.d/brew.sh
@@ -1,7 +1,7 @@
 export HOMEBREW_NO_ANALYTICS=1
 
 if [ -d "${HOME}/.linuxbrew" ]; then
-    eval $(${HOME}/.linuxbrew/bin/brew shellenv)
+	eval $(${HOME}/.linuxbrew/bin/brew shellenv)
 elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
 	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
