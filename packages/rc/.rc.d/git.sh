cb0732d91bfb231bae8556fb3ddc96160e23134d
1efty <git+me@1efty.cloud>
2021-07-03 10:37:49 -0500

    git: update helper functions

--- packages/rc/.rc.d/git.sh
+++ packages/rc/.rc.d/git.sh
@@ -4,18 +4,18 @@ function gitmergehook() {
 	git fetch -p
 }
 
-# get all commiters in repository
-function gitcommitters() {
+# get all commit authors in repository
+function git_authors() {
 	git log --pretty="%an %ae%n%cn %ce" | sort | uniq
 }
 
 # remove a specific file from history
-function gitremovefilefromhistory() {
+function git_purge_file_from_history() {
 	git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
 }
 
 # remove a specific directory from history
-function gitremovedirfromhistory() {
+function git_purge_dir_from_history() {
 	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
 }
 
@@ -23,4 +23,20 @@ function gitremovedirfromhistory() {
 function gitlocalconfig() {
 	git config user.name "$(git config --get user.name)"
 	git config user.email "$(git config --get user.email)"
+}
+
+function git_change_commit_author() {
+	local WRONG_EMAIL="$1" NEW_NAME="$2" NEW_EMAIL="$3"
+	git filter-branch --env-filter "
+if [ \"\$GIT_COMMITTER_EMAIL\" = \"$WRONG_EMAIL\" ]
+then
+    export GIT_COMMITTER_NAME=\"$NEW_NAME\"
+    export GIT_COMMITTER_EMAIL=\"$NEW_EMAIL\"
+fi
+if [ \"\$GIT_AUTHOR_EMAIL\" = \"$WRONG_EMAIL\" ]
+then
+    export GIT_AUTHOR_NAME=\"$NEW_NAME\"
+    export GIT_AUTHOR_EMAIL=\"$NEW_EMAIL\"
+fi
+" --tag-name-filter cat -- --branches --tags
 }
