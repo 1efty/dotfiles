e70afa608acac5f34b1d370db37e4158652b49f3
1efty <git+me@1efty.cloud>
2021-07-05 23:53:02 -0500

    fix: do not try to source bash_completion.d dir

--- packages/bash/.bashrc.d/bash-completions.sh
+++ packages/bash/.bashrc.d/bash-completions.sh
@@ -8,7 +8,7 @@ if type brew &>/dev/null; then
 	if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
 		source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
 	else
-		for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d"*; do
+		for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
 			[[ -r "$COMPLETION" ]] && source "$COMPLETION"
 		done
 	fi
