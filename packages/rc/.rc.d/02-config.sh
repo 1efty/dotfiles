9576993a5d9933f6c5f9850a63cd2611d185f7a0
1efty <git+me@1efty.cloud>
2021-07-11 00:58:41 -0500

    environment: set SHELL

--- packages/rc/.rc.d/02-config.sh
+++ packages/rc/.rc.d/02-config.sh
@@ -6,6 +6,8 @@ export OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 export EDITOR="vim"
 
+export SHELL="$(which zsh)"
+
 # disable BASH deprecation warnings on `darwin`
 if [ "$OS" = "darwin" ]; then
 	export BASH_SILENCE_DEPRECATION_WARNING=1
--- packages/rc/.rc.d/toolbox.sh
+++
@@ -1,3 +0,0 @@
-if command -v toolbox >/dev/null 2>&1; then
-	alias toolbox="env SHELL=/bin/zsh toolbox"
-fi
