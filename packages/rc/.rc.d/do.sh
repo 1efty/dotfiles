e1d96b5c9e9e6ba3a2017218341b152a9bd3432a
1efty <git+me@1efty.cloud>
2021-07-13 22:40:26 -0500

    doctl: hide behind flag

--- packages/rc/.rc.d/do.sh
+++ packages/rc/.rc.d/do.sh
@@ -1,20 +1,23 @@
-if [ "${OS}" = "darwin" ]; then
-	export DIGITALOCEAN_CONFIG="${HOME}/Library/Application Support/doctl/config.yaml"
-elif [ "${OS}" = "linux" ]; then
-	export DIGITALOCEAN_CONFIG="${HOME}/.config/doctl/config.yaml"
-fi
+if command -v doctl >/dev/null; then
 
-# setup environment for digitalocean communication
-# requires yq > 4.0 & doctl auth init
-function doenv() {
-	if command -v yq >/dev/null; then
-		if [ -f "${DIGITALOCEAN_CONFIG}" ]; then
-			export DIGITALOCEAN_ACCESS_TOKEN="$(yq eval .access-token "${DIGITALOCEAN_CONFIG}")"
-			export DO_TOKEN="${DIGITALOCEAN_ACCESS_TOKEN}"
-		else
-			echo "${DIGITALOCEAN_CONFIG} is not available"
-		fi
-	else
-		echo "yq is not installed"
+	if [ "${OS}" = "darwin" ]; then
+		export DIGITALOCEAN_CONFIG="${HOME}/Library/Application Support/doctl/config.yaml"
+	elif [ "${OS}" = "linux" ]; then
+		export DIGITALOCEAN_CONFIG="${HOME}/.config/doctl/config.yaml"
 	fi
-}
+
+	# setup environment for digitalocean communication
+	# requires yq > 4.0 & doctl auth init
+	function doenv() {
+		if command -v yq >/dev/null; then
+			if [ -f "${DIGITALOCEAN_CONFIG}" ]; then
+				export DIGITALOCEAN_ACCESS_TOKEN="$(yq eval .access-token "${DIGITALOCEAN_CONFIG}")"
+				export DO_TOKEN="${DIGITALOCEAN_ACCESS_TOKEN}"
+			else
+				echo "${DIGITALOCEAN_CONFIG} is not available"
+			fi
+		else
+			echo "yq is not installed"
+		fi
+	}
+fi
